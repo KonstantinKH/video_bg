86e052898bc2e953ab3cb75a81b795b4c4ccf6f2
KonstantinKH <kkhomenko9@gmail.com>
2016-11-21 16:22:11 +0200

    added 2

---
+++ js/custom.js
@@ -0,0 +1,53 @@
+$(document).ready(function(){
+
+
+	// menu button
+
+
+	$(".toggle-button").click(function(){
+
+		$(".header-block nav").toggleClass("active-menu");
+	});
+
+	$(".toggle-button").click(function() {
+
+	  $(".sandwich").toggleClass("active");
+
+	});
+
+	$("nav a").click(function(){
+
+		$(".header-block nav").toggleClass("active-menu");
+		$(".sandwich").toggleClass("active");
+
+	});
+
+	// .content-wrap height
+
+
+	$( window ).resize(function() {
+
+		var headerHeight = $(".header-block").innerHeight();
+
+		var bodyHeight = $(window).innerHeight();
+		
+		var contentHeight = bodyHeight - headerHeight
+		
+		$(".content-wrap").height(contentHeight);
+
+	//alert(bodyHeight)
+
+	}).resize();
+
+
+}); // ready()
+
+
+$(window).load(function() {
+
+	$(".loader_inner").fadeOut();
+	$(".loader").delay(400).fadeOut("slow");
+
+
+
+}); // load
